df3d63ffeb245edd3dbac1439cf4b84669138d74
SYLVESTER AZUBUINE <sylvest.ibm.53@gmail.com>
2022-03-29 17:45:30 +0100

    This is the second version of the code

--- Code main.cpp
+++ Code main.cpp
@@ -27,6 +27,7 @@ void convert();
 void delivery();
 void by();
 void flowrate();
+void lcdprep();
 
 //Global vairables Declaration and Initialisation
 int container0=0, digitone=0, container1=0, digittwo=0, container2=0;
@@ -35,68 +36,97 @@ int digitfive=0, container5=0, digitsix=0, container6=0;
 int digitseven=0, container7=0;
 int num, check=3545202;
 float cost, tickert;
+bool flag1=0, flag2=0, flag3=0;     //allows some functions to run once
 Timer t;
 Ticker t1;
 Timeout t2;
-InterruptIn btn(BUTTON1);
 
 //Main function where the algo is buit
 int main(){
-    while(IND1==0){
-        lcd.cls();
-        lcd.locate(0,0);
-        lcd.printf("Switch on the Dispenser");
-        if(holster==1){
-            printf("Attentant lifted the nozzle from the holster\n");
-            attendant();
-            underscore();
-            firstdigit();
-            seconddigit();
-            thirddigit();
-            fourthdigit();
-            fifthdigit();
-            sixthdigit();
-            seventhdigit();
-            convert();
-            wait(0.2);
-        }else{
-            IND2.write(false);
-            wait(0.2);
+    while(IND1==0 && holster==0){
+        t1.attach(callback(&by), 500.0f);   //Remove once you can detach ticker interrupt
+        while(IND1==0){
+            lcdprep();
+            lcd.printf("Switch on the Dispenser");
+            if(holster==1){
+                printf("Attentant lifted the nozzle from the holster\n");
+                attendant();
+                underscore();
+                firstdigit();
+                seconddigit();
+                thirddigit();
+                fourthdigit();
+                fifthdigit();
+                sixthdigit();
+                seventhdigit();
+                convert();
+                wait(0.2);
+            }else{
+                IND2.write(false);
+                wait(0.2);
+            }
         }
+        if(IND1==1){
+            lcdprep();
+            lcd.printf("Press the enter button to \ninitiate fuel delivery");
+        }
+        while(holster==1){
+            if(PB3==1){
+            lcdprep();
+            lcd.printf("Fuel: %.2f ltrs\nCumulative cost: GBP %.2f",tickert,cost);
+            printf("Delivery Initiated.\n");
+            flowrate();     //add this funtion to when the enter button is pressed
+            }
+            if(cost<150 && holster==0){   //allows metering to work if code starts afresh
+            IND1=0;
+            printf("Pump has been turned off\n");
+            printf("Total cost of fuel dispensed: GPB %.2f\n", cost);
+            wait(1);
+            tickert=0;
+            cost=0;
+            flag1=0;
+            flag2=0;
+            }else if(cost>150 && holster==1 && flag3==0){   //keeps cost on pc screen
+            IND1=0;
+            printf("Maximum volume of fuel has been dispensed\n");
+            printf("Total cost of fuel dispensed: GPB %.2f\n", cost);
+            flag3=1;        //makes cost display once
+            }
+        wait_ms(2);
+        }
+        if(cost>150 && holster==0){   //allows metering to work if code starts afresh
+            IND1=0;
+            printf("Pump has been turned off\n");
+            wait(1);
+            tickert=0;
+            cost=0;
+            flag1=0;
+            flag2=0;
+            flag3=0;
+        }
+        wait(1);
     }
-    if(IND1==1){
-        lcd.cls();
-        lcd.locate(0,0);
-        lcd.printf("Turn potentiometer to start fuel  delivery");
-    }
-    flowrate();         //add this funtion to when the enter button is pressed
-    if(cost>150 && holster==1){
-        printf("Maximum volume of fuel has been dispensed\n");
-        printf("Total cost of fuel dispensed: %f\n", cost);
-    }else if(holster==0){
-        IND1=0;
-        printf("Pump has been turned off\n");
-        printf("Total cost of fuel dispensed: %f\n", cost);
-    }
-    
 }
 
 
 
-
 //Function definitions
+//Prepares LCD for display
+void lcdprep(){
+    lcd.cls();
+    lcd.locate(0,0);
+}
 //Requests code from user
 void attendant(){
-    lcd.locate(0,0);
+    lcdprep();
     lcd.printf("Enter your pesonal code: \n");
-    }
+}
 
 //displays "_" on the lcd
 void underscore(){
     while(container0==0 && holster==1){
         if(PB3==1){                     //Placing _ on the lcd
-            lcd.cls();
-            lcd.locate(0,0);
+            lcdprep();
             lcd.printf("_");
             container0=1;
             printf("Code authentication started\n");
@@ -268,35 +298,29 @@ void convert(){
     num=num+digitsix;
     num=num*10;
     num=num+digitseven;
-    if(num==check){
+    if(num==check){             //Auth code but enable program to start afresh
        IND1.write(1);
        IND2.write(0);
+       container0=0; digitone=0; container1=0; digittwo=0; container2=0;
+       digitthree=0; container3=0; digitfour=0; container4=0;
+       digitfive=0; container5=0; digitsix=0; container6=0;
+       digitseven=0; container7=0;
        printf("Attendant's code is correct.\n");
        wait(1);
-       printf("Delivery Initiated.\n");
-    }else{                      //This code below enables authentication to start afresh    
+    }else if(container0!=0){    //Enables restart of auth when code is wrong    
        IND2.write(1);
        container0=0; digitone=0; container1=0; digittwo=0; container2=0;
        digitthree=0; container3=0; digitfour=0; container4=0;
        digitfive=0; container5=0; digitsix=0; container6=0;
        digitseven=0; container7=0;
        printf("Attendant's code is wrong.\n");
+    }else{
+       IND2.write(1);
+       printf("Attendant returned nozzle to the holster\n");
     }
 }
-
-//invoking interrupt from button
-/*void delivery(){
-        lcd.cls();
-        lcd.locate(1,1);
-        lcd.printf("Fuel delivered:---litres");
-        lcd.locate(1,15);
-        lcd.printf("Accumulative cost: $%f    ",pot.read());
-        
-        wait(0.2);
-    }*/
-
 //blink yellow led, calculates cost, tells casher when pump is running and when paused
-void by(){                      
+void by(){
     if(cost<150.1 && holster==1){
         pump = !pump;
         tickert=tickert+0.1;
@@ -304,71 +328,52 @@ void by(){
     }else{
         pump.write(0);
     }
-    if(pot>0 && holster==1 && cost<150.1){
+    if(pot>0 && holster==1 && cost<150.1 && flag1==0){
         printf("Fuel is deivering\n");
-    }else if(pot==0 && holster==1 && cost<150.1){
+        flag1=1;
+    }else if(pot==0 && holster==1 && cost<150.1 && flag2==0){
         printf("Fuel delivery is paused\n");
+        flag2=1;
     }
 }
-
-
 //led blinking to indicate flow rate
 void flowrate(){
     while (IND1==1 && holster==1 && cost<150.1) {
         if(pot*5>0 && pot*5<1 || pot*5==1){
             t1.attach(callback(&by), 1.0f);
-            lcd.cls();
-            lcd.locate(1,1);
-            lcd.printf("Fuel: %.2f ltrs",tickert);
-            lcd.locate(1,10);
-            lcd.printf("Cumulative cost: GBP %.2f",cost);
+            lcdprep();
+            lcd.printf("Fuel: %.2f ltrs\nCumulative cost: GBP %.2f",tickert,cost);
             wait(1.0);
         }else if(pot*5>1 && pot*5<2 || pot*5==2){
             t1.attach(callback(&by), 0.8f);
-            lcd.cls();
-            lcd.locate(1,1);
-            lcd.printf("Fuel: %.2f ltrs",tickert);
-            lcd.locate(1,10);
-            lcd.printf("Cumulative cost: GBP %.2f",cost);
+            lcdprep();
+            lcd.printf("Fuel: %.2f ltrs\nCumulative cost: GBP %.2f",tickert,cost);
             wait(0.8);
         }else if(pot*5>2 && pot*5<3 || pot*5==3){
             t1.attach(callback(&by), 0.6f);
-            lcd.cls();
-            lcd.locate(1,1);
-            lcd.printf("Fuel: %.2f ltrs",tickert);
-            lcd.locate(1,10);
-            lcd.printf("Cumulative cost: GBP %.2f",cost);
+            lcdprep();
+            lcd.printf("Fuel: %.2f ltrs\nCumulative cost: GBP %.2f",tickert,cost);
             wait(0.6);
         }else if(pot*5>3 && pot*5<4 || pot*5==4){
             t1.attach(callback(&by), 0.4f);
-            lcd.cls();
-            lcd.locate(1,1);
-            lcd.printf("Fuel: %.2f ltrs",tickert);
-            lcd.locate(1,10);
-            lcd.printf("Cumulative cost: GBP %.2f",cost);
+            lcdprep();
+            lcd.printf("Fuel: %.2f ltrs\nCumulative cost: GBP %.2f",tickert,cost);
             wait(0.4);
         }else if(pot*5>4 && pot*5<5){
             t1.attach(callback(&by), 0.2f);
-            lcd.cls();
-            lcd.locate(1,1);
-            lcd.printf("Fuel: %.2f ltrs",tickert);
-            lcd.locate(1,10);
-            lcd.printf("Cumulative cost: GBP %.2f",cost);
+            lcdprep();
+            lcd.printf("Fuel: %.2f ltrs\nCumulative cost: GBP %.2f",tickert,cost);
             wait(0.2);
         }else if(pot*5>4 && pot*5<5 || pot*5==5){
             t1.attach(callback(&by), 0.1f);
-            lcd.cls();
-            lcd.locate(1,1);
-            lcd.printf("Fuel: %.2f ltrs",tickert);
-            lcd.locate(1,10);
-            lcd.printf("Cumulative cost: GBP %.2f",cost);
+            lcdprep();
+            lcd.printf("Fuel: %.2f ltrs\nCumulative cost: GBP %.2f",tickert,cost);
             wait(0.1);
         }else{
-            t1.attach(callback(&by), 5.0f);    //the time lower than wait time, so it can attach
+            t1.attach(callback(&by), 500.0f);    //the time lower than wait time, so it can attach
             pump.write(0);  
             wait_us(0.2);           //wait time is far lower than attach time. (use detach function in this block)
         }
-        //wait(0.2);
         //wait_ms(osWaitForever);
     }
 }
